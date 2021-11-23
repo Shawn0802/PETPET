b018e36cec07af02597f54e3c64b4d0aa33ac2da
shawn <qman19930802@gmail.com>
2021-11-23 15:55:58 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,33 @@
+//swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    //autoplay
+    autoplay: {
+        delay: 3000,
+      },
+
+});
+
+
+//drop-down
+$(document).ready(function(){
+    $('.link-arrow').click(function(event){
+    event.preventDefault();
+    var n = $(document).height();
+    $('html, body').animate({ scrollTop: 750 }, 50);
+  })
+  })
+
+
+
+
+
+  
